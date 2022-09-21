02663e12f851845d4a24835480473a21d8ce584e
Latgardi <saikinmirai@gmail.com>
2022-09-21 23:26:19 +0300

    ex2-95

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,7 +1,12 @@
 <?php
+/*Bitrix\Main\Loader::registerAutoLoadClasses(null, array(
+	'\EventHandlers' => '/local/php_interface/eventhandlers.php',
+));*/
+
 AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("EventHandlers", "deactivateBlockElement"));
 AddEventHandler("main", "OnEpilog", Array("EventHandlers", "writeLog404"));
 AddEventHandler("main", "OnBeforeEventAdd", Array("EventHandlers", "feedbackFormChangeAuthor"));
+AddEventHandler('main', 'OnBuildGlobalMenu', Array("EventHandlers", "simplifiedGlobalMenu"));
 
 class EventHandlers
 {
@@ -27,7 +32,7 @@ class EventHandlers
 				"AUDIT_TYPE_ID" => "ERROR_404",
 				"MODULE_ID" => "MAIN",
 				"DESCRIPTION" => $pageURL
-				));
+			));
 		}
 	}
 
@@ -51,6 +56,33 @@ class EventHandlers
 				"DESCRIPTION" => "Замена данных в отсылаемом письме – $author"
 			));
 
+		}
+	}
+
+
+	function simplifiedGlobalMenu(&$aGlobalMenu, &$aModuleMenu)
+	{
+		global $USER;
+		$userID = $USER->GetID();
+		if (in_array(6, $USER::GetUserGroup($userID))) {
+			$deleted = array();
+			foreach (array_keys($aGlobalMenu) as $key) {
+				if ($key == "global_menu_content") {
+					continue;
+				}
+				unset($aGlobalMenu[$key]);
+				$deleted[] = $key;
+			}
+
+			foreach ($aModuleMenu as $key => $item) {
+				if (in_array($item["parent_menu"], $deleted, true)) {
+					unset($aModuleMenu[$key]);
+				}
+				if ($item["parent_menu"]=="global_menu_content" && $item["text"]=="Новости") {
+					continue;
+				}
+				unset($aModuleMenu[$key]);
+			}
 		}
 	}
 }
