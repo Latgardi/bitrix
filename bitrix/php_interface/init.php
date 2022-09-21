a015ce2c83deffdd2c220db7caf22e06a12c0768
Latgardi <saikinmirai@gmail.com>
2022-09-21 16:17:53 +0300

    ex2-51

--- bitrix/php_interface/init.php
+++ bitrix/php_interface/init.php
@@ -1,10 +1,11 @@
 <?php
-AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("EventHandlers", "deactivateBlockElementHandler"));
-AddEventHandler("main", "OnEpilog", Array("EventHandlers", "writeLog404Handler"));
+AddEventHandler("iblock", "OnBeforeIBlockElementUpdate", Array("EventHandlers", "deactivateBlockElement"));
+AddEventHandler("main", "OnEpilog", Array("EventHandlers", "writeLog404"));
+AddEventHandler("main", "OnBeforeEventAdd", Array("EventHandlers", "feedbackFormChangeAuthor"));
 
 class EventHandlers
 {
-	public function deactivateBlockElementHandler(array &$arFields)
+	public function deactivateBlockElement(array &$arFields)
 	{
 		if ($arFields["IBLOCK_ID"] == 2 && $arFields["ACTIVE"] == "N") {
 			$count = $arFields["SHOW_COUNTER"];
@@ -16,7 +17,7 @@ class EventHandlers
 		}
 	}
 
-	public function writeLog404Handler()
+	public function writeLog404(): void
 	{
 		if (defined('ERROR_404')) {
 			global $APPLICATION;
@@ -27,6 +28,29 @@ class EventHandlers
 				"MODULE_ID" => "MAIN",
 				"DESCRIPTION" => $pageURL
 				));
+		}
+	}
+
+	public function feedbackFormChangeAuthor(string &$event, string &$lid, array &$arFields): void
+	{
+		if ($event == "FEEDBACK_FORM") {
+			global $USER;
+			if (!$USER->isAuthorized()) {
+				$author = $arFields["AUTHOR"];
+				$author = "Пользователь не авторизован, данные из формы: $author";
+				$arFields["AUTHOR"] = $author;
+			} else {
+				$author = $arFields["AUTHOR"];
+				$author = "Пользователь авторизован: {$USER->GetID()} ({$USER->GetLogin()}) {$USER->getFullName()}, данные из формы: $author";
+				$arFields["AUTHOR"] = $author;
+			}
+			CEventLog::Add(array(
+				"SEVERITY" => "SECURITY",
+				"AUDIT_TYPE_ID" => "CHANGE_MAIL_DATA",
+				"MODULE_ID" => "MAIN",
+				"DESCRIPTION" => "Замена данных в отсылаемом письме – $author"
+			));
+
 		}
 	}
 }
---
+++ ex2/feedback/index.php
@@ -0,0 +1,8 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Форма обратной связи");
+?>
+
+Text here....
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
